b3ff39d457f46d094673bd2b1d7e10fc48993df3
dev-medina29 <mmbedi@byupathway.edu>
2026-08-08 11:52:44 +0100

    login ok

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-// export { auth as proxy } from "@/auth";
+ export { auth as middleware } from "@/auth";
 // export const config = {
 //   matcher: ["/admin/meetings/:path*"], // protect all admin meeting routes
 // };
